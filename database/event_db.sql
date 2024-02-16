7b0a06950c3e27fd1b12a2c850511f99776004d9
kiarastylin <cattygeor788@gmail.com>
2024-02-16 14:44:13 +0300

    new

--- database/event_db.sql
+++ database/event_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2021 at 10:21 AM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.7
+-- Generation Time: Feb 16, 2024 at 12:41 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,15 +37,18 @@ CREATE TABLE `event_audience` (
   `status` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `event_audience`
 --
 
 INSERT INTO `event_audience` (`id`, `event_id`, `name`, `contact`, `email`, `remarks`, `status`, `date_created`, `date_updated`) VALUES
-(1, 1, 'Claire Blake', '09123456789', 'cblake@sample.com', '09123456789', 0, '2021-07-26 11:59:02', NULL),
-(3, 1, 'Mike Williams', '09123456789', 'mwilliams@sample.com', 'Sample', 0, '2021-07-26 15:33:32', NULL);
+(5, 1, 'Catherine Masongo', '23345678', 'cattygeor788@gmail.com', 'asdfghj', 0, '2024-02-06 16:13:32', NULL),
+(6, 4, 'Catherine George Masongo ', '0746173264', 'cattygeor788@gmail.com', 'tyuio', 0, '2024-02-06 16:49:06', NULL),
+(7, 5, 'Catherine Masongo', '0746173264', 'cattygeor788@gmail.com', 'ujnmkl', 0, '2024-02-12 14:34:21', NULL),
+(8, 5, 'razac', '1234567', 'tygtrf@gmail.com', 'ASDFGHJ', 0, '2024-02-12 14:57:42', NULL),
+(9, 5, 'TYUI', '2345678', 'WERTYUI@gmail.com', 'dfghjk', 0, '2024-02-12 15:00:50', NULL);
 
 -- --------------------------------------------------------
 
@@ -65,15 +68,15 @@ CREATE TABLE `event_list` (
   `user_id` int(30) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp(),
   `date_update` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `event_list`
 --
 
 INSERT INTO `event_list` (`id`, `title`, `description`, `venue`, `limit_registration`, `datetime_start`, `datetime_end`, `limit_time`, `user_id`, `date_created`, `date_update`) VALUES
-(1, 'Sample 101', 'Sample Event Only', 'Venue 101', 0, '2021-07-26 13:00:00', '2021-07-26 17:00:00', 0, 3, '2021-07-26 11:10:12', '2021-07-26 11:29:25'),
-(3, 'Sample 102', 'Sample only', 'Venue 102', 1, '2021-07-26 15:00:00', '2021-07-26 17:30:00', 30, 3, '2021-07-26 13:36:43', NULL);
+(4, 'Sample 103', 'asdfghjk', '103', 0, '2024-02-06 16:35:00', '2024-02-06 17:40:00', 0, 4, '2024-02-06 16:36:00', NULL),
+(5, 'Event 103', 'rtyui', '123', 1, '2024-02-12 14:32:00', '2024-02-13 14:32:00', 120, 4, '2024-02-12 14:33:40', NULL);
 
 -- --------------------------------------------------------
 
@@ -87,15 +90,18 @@ CREATE TABLE `registration_history` (
   `audience_id` int(30) NOT NULL,
   `user_id` int(30) NOT NULL,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `registration_history`
 --
 
 INSERT INTO `registration_history` (`id`, `event_id`, `audience_id`, `user_id`, `date_created`) VALUES
-(1, 1, 3, 3, '2021-07-26 15:35:33'),
-(2, 1, 1, 3, '2021-07-26 15:40:04');
+(1, 1, 3, 3, '2024-02-02 15:35:33'),
+(2, 1, 1, 3, '2024-02-01 15:40:04'),
+(3, 5, 7, 4, '2024-02-12 14:54:36'),
+(4, 5, 8, 4, '2024-02-12 14:59:36'),
+(5, 5, 9, 4, '2024-02-12 15:03:44');
 
 -- --------------------------------------------------------
 
@@ -107,14 +113,14 @@ CREATE TABLE `system_info` (
   `id` int(30) NOT NULL,
   `meta_field` text NOT NULL,
   `meta_value` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `system_info`
 --
 
 INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
-(1, 'name', 'Salstech System with QR Code - PHP'),
+(1, 'name', 'Salstech System'),
 (2, 'address', 'Tanzania'),
 (3, 'contact', '+1234567890'),
 (4, 'email', 'info@sample.com'),
@@ -139,15 +145,16 @@ CREATE TABLE `users` (
   `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=Admin,2=Registrar',
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
-(1, 'Adminstrator', 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1627261440_avatar.png', NULL, 1, '2021-01-20 14:02:37', '2021-07-26 09:57:03'),
-(3, 'John', 'Smith', 'jsmith', '39ce7e2a8573b41ce73b5ba41617f8f7', 'uploads/1627264800_avatar.png', NULL, 2, '2021-07-26 10:00:18', '2021-07-26 10:04:53');
+(1, 'Adminstrator', 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1627261440_avatar.png', NULL, 1, '2024-01-31 14:02:37', '2024-01-31 09:57:03'),
+(3, 'John', 'Smith', 'jsmith', '39ce7e2a8573b41ce73b5ba41617f8f7', 'uploads/1627264800_avatar.png', NULL, 2, '2024-02-01 10:00:18', '2024-02-01 10:04:53'),
+(4, 'Catherine', 'Masongo', 'kate', '4460a634c2b3cc9e42f5288fbc5d31f9', NULL, NULL, 2, '2024-02-06 16:15:31', NULL);
 
 --
 -- Indexes for dumped tables
@@ -191,19 +198,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `event_audience`
 --
 ALTER TABLE `event_audience`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `event_list`
 --
 ALTER TABLE `event_list`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `registration_history`
 --
 ALTER TABLE `registration_history`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `system_info`
@@ -215,7 +222,7 @@ ALTER TABLE `system_info`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
